580d94b17d29b2053f42d17220722f4eee50f2bd
Alexandra <allexa.msv@gmail.com>
2025-09-30 00:35:10 +0300

    add script

---
+++ start_bot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PROJECT_DIR=~/VKRTeamBot
+REPO_URL="https://github.com/masllova/BKRTeamCode"
+
+echo "🚀 Старт скрипта запуска VKRTeamBot..."
+
+if [ ! -d "$PROJECT_DIR" ]; then
+    git clone $REPO_URL $PROJECT_DIR
+fi
+
+cd $PROJECT_DIR || exit
+git pull
+
+# Виртуальное окружение
+if [ ! -d "venv" ]; then
+    echo "⚙️ Создаём виртуальное окружение..."
+    python3 -m venv venv
+    echo "✔️ Виртуальное окружение создано!"
+fi
+echo "⚙️ Активируем виртуальное окружение..."
+source venv/bin/activate
+pip install -r requirements.txt
+echo "✔️ Зависимости установлены!"
+
+# Токен бота
+if [ -z "$TELEGRAM_TOKEN" ]; then
+    read -p "Введите токен бота: " BOT_TOKEN
+    export TELEGRAM_TOKEN="$BOT_TOKEN"
+fi
+
+# Запуск бота
+echo "⚙️ Ещё немного"
+python main.py
