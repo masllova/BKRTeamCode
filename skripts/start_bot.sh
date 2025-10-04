01ac6768d7a4debdda82bd8b8fd14f301f94a376
Alexandra <allexa.msv@gmail.com>
2025-10-04 15:30:39 +0300

    check db password in start skript

--- skripts/start_bot.sh
+++ skripts/start_bot.sh
@@ -5,6 +5,7 @@ REPO_URL="https://github.com/masllova/VKRTeamCode"
 
 echo "🚀 Запускаем код для VKRTeamBot..."
 
+# Клонирование, если проекта ещё нет
 if [ ! -d "$PROJECT_DIR" ]; then
     git clone $REPO_URL $PROJECT_DIR
 fi
@@ -18,17 +19,26 @@ if [ ! -d "venv" ]; then
     python3 -m venv venv
     echo "✔️ Виртуальное окружение создано!"
 fi
+
 echo "⚙️ Активируем виртуальное окружение..."
 source venv/bin/activate
+
 pip install -r requirements.txt
 echo "✔️ Зависимости установлены!"
 
-# Токен бота
+# --- 🔐 Токен бота ---
 if [ -z "$TELEGRAM_TOKEN" ]; then
     read -p "Введите токен бота: " BOT_TOKEN
     export TELEGRAM_TOKEN="$BOT_TOKEN"
 fi
 
+# --- 🔑 Пароль к БД ---
+if [ -z "$VKR_DB_PASSWORD" ]; then
+    read -s -p "Введите пароль к базе данных: " DB_PASSWORD
+    echo ""
+    export VKR_DB_PASSWORD="$DB_PASSWORD"
+fi
+
 # Запуск бота
-echo "⚙️ Ещё немного"
+echo "⚙️ Ещё немного..."
 python main.py
