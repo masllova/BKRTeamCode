3585101d8df408353b939364d34f826195c97a24
Alexandra <allexa.msv@gmail.com>
2025-10-05 17:18:10 +0300

    replace callback handler

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters
 from handlers.start import start
-from handlers.registration import handle_text, handle_role_callback, handle_stage_callback
+from handlers.registration import handle_text, handle_callback
 from config import TOKEN
 
 def main():
@@ -9,8 +9,7 @@ def main():
     app.add_handler(CommandHandler("start", start))
 
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
-    app.add_handler(CallbackQueryHandler(handle_role_callback))
-    app.add_handler(CallbackQueryHandler(handle_stage_callback))
+    app.add_handler(CallbackQueryHandler(handle_callback))
 
     print("🚀 Бот запущен...")
     app.run_polling()
