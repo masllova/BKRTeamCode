d27a7b101c0bbbf8a122c26f174aad26683af8fb
Alexandra <allexa.msv@gmail.com>
2025-10-20 01:51:49 +0300

    update queries requests

--- db/queries_requests.py
+++ db/queries_requests.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 from db.connection import conn, cursor
 
-def add_request(sender_id: int, receiver_id: int):
+def add_request(sender_id: int, receiver_id: int, topic: str):
     cursor.execute(
         """
-        INSERT INTO requests (sender_id, receiver_id, status, created_at)
-        VALUES (%s, %s, 'ожидает', %s)
+        INSERT INTO requests (sender_id, receiver_id, status, topic, created_at)
+        VALUES (%s, %s, 'ожидает', %s, %s)
         RETURNING id;
         """,
-        (sender_id, receiver_id, datetime.utcnow())
+        (sender_id, receiver_id, topic, datetime.utcnow())
     )
     request_id = cursor.fetchone()[0]
     conn.commit()
@@ -16,7 +16,7 @@ def add_request(sender_id: int, receiver_id: int):
 
 def get_incoming_requests(user_id: int, last_id: int | None = None) -> list[dict] | None:
     sql = """
-        SELECT id, sender_id, status, created_at
+        SELECT id, sender_id, topic, status, created_at
         FROM requests
         WHERE receiver_id = %s
     """
@@ -38,14 +38,15 @@ def get_incoming_requests(user_id: int, last_id: int | None = None) -> list[dict
         requests.append({
             "id": r[0],
             "sender_id": r[1],
-            "status": r[2],
-            "created_at": r[3],
+            "topic": r[2],
+            "status": r[3],
+            "created_at": r[4],
         })
     return requests
 
 def get_outgoing_requests(user_id: int, last_id: int | None = None) -> list[dict] | None:
     sql = """
-        SELECT id, receiver_id, status, created_at
+        SELECT id, receiver_id, topic, status, created_at
         FROM requests
         WHERE sender_id = %s
     """
@@ -67,8 +68,9 @@ def get_outgoing_requests(user_id: int, last_id: int | None = None) -> list[dict
         requests.append({
             "id": r[0],
             "receiver_id": r[1],
-            "status": r[2],
-            "created_at": r[3],
+            "topic": r[2],
+            "status": r[3],
+            "created_at": r[4],
         })
     return requests
 
