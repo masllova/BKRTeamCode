71e24c630e9cf9ef9917d3d1acc753b09d226301
Alexandra <allexa.msv@gmail.com>
2025-10-27 00:58:04 +0300

    replace add emoji

--- keyboards/settings.py
+++ keyboards/settings.py
@@ -11,31 +11,31 @@ def make_student_settings_keyboard(has_faculty, has_department, has_specialty, h
     edit_buttons = []
     add_buttons = []
 
-    edit_buttons.append([InlineKeyboardButton("Ступень образования ✅", callback_data=f"student_stage")])
-    edit_buttons.append([InlineKeyboardButton("Учебное заведение ✅", callback_data=f"student_university")])
+    edit_buttons.append([InlineKeyboardButton("Ступень образования ✔️", callback_data=f"student_stage")])
+    edit_buttons.append([InlineKeyboardButton("Учебное заведение ✔️", callback_data=f"student_university")])
 
     if has_faculty:
-        edit_buttons.append([InlineKeyboardButton("Факультет ✅", callback_data=f"student_faculty")])
+        edit_buttons.append([InlineKeyboardButton("Факультет ✔️", callback_data=f"student_faculty")])
     else:
         add_buttons.append([InlineKeyboardButton("Добавить факультет", callback_data=f"student_faculty")])
     if has_department:
-        edit_buttons.append([InlineKeyboardButton("Кафедра/Направление ✅", callback_data=f"student_department")])
+        edit_buttons.append([InlineKeyboardButton("Кафедра/Направление ✔️", callback_data=f"student_department")])
     else:
         add_buttons.append([InlineKeyboardButton("Добавить кафедра/направление", callback_data=f"student_department")])
     if has_specialty:
-        edit_buttons.append([InlineKeyboardButton("Специальность ✅", callback_data=f"student_specialty")])
+        edit_buttons.append([InlineKeyboardButton("Специальность ✔️", callback_data=f"student_specialty")])
     else:
         add_buttons.append([InlineKeyboardButton("Добавить специальность", callback_data=f"student_specialty")])
     if has_articles:
-        edit_buttons.append([InlineKeyboardButton("Статьи ✅", callback_data=f"articles")])
+        edit_buttons.append([InlineKeyboardButton("Статьи ✔️", callback_data=f"articles")])
     else:
         add_buttons.append([InlineKeyboardButton("Добавить cтатьи", callback_data=f"articles")])
     if has_interests:
-        edit_buttons.append([InlineKeyboardButton("Научные интересы ✅", callback_data=f"research_interests")])
+        edit_buttons.append([InlineKeyboardButton("Научные интересы ✔️", callback_data=f"research_interests")])
     else:
         add_buttons.append([InlineKeyboardButton("Добавить научные интересы", callback_data=f"research_interests")])
     if has_email:
-        edit_buttons.append([InlineKeyboardButton("Почта ✅", callback_data=f"email")])
+        edit_buttons.append([InlineKeyboardButton("Почта ✔️", callback_data=f"email")])
     else:
         add_buttons.append([InlineKeyboardButton("Добавить почту", callback_data=f"email")])
 
@@ -48,23 +48,23 @@ def make_teacher_settings_keyboard(has_degree, has_articles, has_interests, has_
     edit_buttons = []
     add_buttons = []
 
-    edit_buttons.append([InlineKeyboardButton("Должность ✅", callback_data=f"teacher_stage")])
-    edit_buttons.append([InlineKeyboardButton("Научное учреждение ✅", callback_data=f"teacher_university")])
+    edit_buttons.append([InlineKeyboardButton("Должность ✔️", callback_data=f"teacher_stage")])
+    edit_buttons.append([InlineKeyboardButton("Научное учреждение ✔️", callback_data=f"teacher_university")])
 
     if has_degree:
-        edit_buttons.append([InlineKeyboardButton("Степень ✅", callback_data=f"teacher_degree")])
+        edit_buttons.append([InlineKeyboardButton("Степень ✔️", callback_data=f"teacher_degree")])
     else:
         add_buttons.append([InlineKeyboardButton("Добавить степень", callback_data=f"teacher_degree")])
     if has_articles:
-        edit_buttons.append([InlineKeyboardButton("Статьи ✅", callback_data=f"articles")])
+        edit_buttons.append([InlineKeyboardButton("Статьи ✔️", callback_data=f"articles")])
     else:
         add_buttons.append([InlineKeyboardButton("Добавить cтатьи", callback_data=f"articles")])
     if has_interests:
-        edit_buttons.append([InlineKeyboardButton("Научные интересы ✅", callback_data=f"research_interests")])
+        edit_buttons.append([InlineKeyboardButton("Научные интересы ✔️", callback_data=f"research_interests")])
     else:
         add_buttons.append([InlineKeyboardButton("Добавить научные интересы", callback_data=f"research_interests")])
     if has_email:
-        edit_buttons.append([InlineKeyboardButton("Почта ✅", callback_data=f"email")])
+        edit_buttons.append([InlineKeyboardButton("Почта ✔️", callback_data=f"email")])
     else:
         add_buttons.append([InlineKeyboardButton("Добавить почта", callback_data=f"email")])
     
